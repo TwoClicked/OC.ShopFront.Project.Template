f63df818626656d5e5980df96741926d3c01bbc3
diego <Diego/HomeDesktop>
2025-08-30 00:34:46 +0200

    Finished, Product detail/shop page with variants

---
+++ OC.LUAC.UiLayer/DTO/Products/ProductDetailsDtocs.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OC.LUAC.UiLayer.DTO.Products
+{
+    public class ProductDetailsDto
+    {
+        public int Id { get; set; }
+
+        [JsonPropertyName("name_en")]
+        public string Name_En { get; set; } = "";
+
+        [JsonPropertyName("name_de")]
+        public string Name_De { get; set; } = "";
+
+        [JsonPropertyName("description_en")]
+        public string Description_En { get; set; } = "";
+
+        [JsonPropertyName("description_de")]
+        public string Description_De { get; set; } = "";
+
+        public decimal Price { get; set; }
+
+        [JsonPropertyName("thumbnailUrl")]
+        public string? ThumbnailUrl { get; set; }   // 🔹 this was missing!
+
+        [JsonPropertyName("images")]
+        public List<ProductImageDto> Images { get; set; } = new();
+
+        [JsonPropertyName("variants")]
+        public List<ProductVariantDto> Variants { get; set; } = new();
+    }
+}
