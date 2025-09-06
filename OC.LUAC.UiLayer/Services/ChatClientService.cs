a1c6423fd22697c62b65ef7cdf5c7d1ba4cef8a4
diego <Diego/HomeDesktop>
2025-09-06 21:23:49 +0200

    Removal BlazorAPP1 (Wrong Project) start chat client ui

---
+++ OC.LUAC.UiLayer/Services/ChatClientService.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace OC.LUAC.UiLayer.Services
+{
+    public class ChatClientService : IAsyncDisposable
+    {
+
+
+        private readonly HubConnection _connection;
+
+        public ChatClientService(IConfiguration config)
+        {
+
+            
+        }
+
+
+        public async ValueTask DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
